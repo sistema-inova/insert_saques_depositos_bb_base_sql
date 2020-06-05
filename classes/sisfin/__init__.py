50fc2b9f27def97dd7fb8323a5f679a4381ce597
EDUARDO CHIAKI CHUMAN <eduardo.chuman@caixa.gov.br>
2020-06-05 18:44:05 -0300

    finalizacao da primeira versão da ferramenta de captura

--- classes/sisfin/__init__.py
+++ classes/sisfin/__init__.py
@@ -9,7 +9,7 @@ import os
 class Sisfin:
     
     def __init__(self, matricula, senha):
-        os.startfile('sisfin_edp\SISFIN.edp')
+        os.startfile('sisfin_edp\\SISFIN.edp')
         time.sleep(3)
 
         self.system = CreateObject("Extra.System", dynamic=True)
@@ -22,7 +22,7 @@ class Sisfin:
         pyautogui.keyUp('up')
 
         time.sleep(1)
-        self.sisfin_escrever(self.screen, 22,11, matricula)
+        self.sisfin_escrever(22, 11, matricula)
         time.sleep(1)
         self.screen.SendKeys(senha)
         self.screen.SendKeys("<Enter>")
@@ -31,124 +31,126 @@ class Sisfin:
     def sisfin_finalizar(self):  
         os.system("taskkill /f /im EXTRA.EXE")
 
-    def sisfin_voltar_pagina_inicial_cardapio(self, screen, linha):
+    def sisfin_voltar_pagina_inicial_cardapio(self, linha):
         print("")
         # VOLTAR PARA A PÁGINA INICIAL DO CARDÁPIO
-        self.sisfin_escrever(screen, linha, 8, '.')
+        self.sisfin_escrever(linha, 8, '.')
         time.sleep(0.5)
 
-    def sisfin_ler(self, screen, row, col, length):
-        return screen.Area(row, col, row, col+length).value  
+    def sisfin_ler(self, row, col, length):
+        return self.screen.Area(row, col, row, col+length).value  
 
-    def sisfin_escrever(self, screen, row, col, text):
-        screen.row = row
-        screen.col = col
-        screen.SendKeys(text)
+    def sisfin_escrever(self, row, col, text):
+        self.screen.row = row
+        self.screen.col = col
+        self.screen.SendKeys(text)
         time.sleep(0.5)
-        screen.SendKeys("<Enter>")
+        self.screen.SendKeys("<Enter>")
         time.sleep(0.2)
 
-    def vmcy_acessar(self, screen, menu, periodo):
+    def vmcy_acessar(self, menu, periodo):
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 21,8,'VMCY')
+        self.sisfin_escrever(21,8,'VMCY')
         time.sleep(1)
-        while (self.sisfin_ler(screen, 2, 1,7) == 'Cardapio'):
+        while (self.sisfin_ler(2, 1,7) == 'Cardapio'):
             time.sleep(1)
 
-        self.sisfin_escrever(screen, 4, 13, 'T')
+        self.sisfin_escrever(4, 13, 'T')
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 4, 44, menu)
+        self.sisfin_escrever(4, 44, menu)
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 5, 17, 'T')
+        self.sisfin_escrever(5, 17, 'T')
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 5, 60, periodo)
+        self.sisfin_escrever(5, 60, periodo)
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 5, 71, periodo)
+        self.sisfin_escrever(5, 71, periodo)
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 6, 17, 'T')
+        self.sisfin_escrever(6, 17, 'T')
         time.sleep(1.5)
 
-    def vmcy_tratamento_sem_movimentacao(self, screen, celula_planilha):
+    def vmcy_tratamento_sem_movimentacao(self, celula_planilha):
         print("R$ -") 
         celula_planilha = 0
-        self.sisfin_escrever(screen, 23, 50, '.')
+        self.sisfin_escrever(23, 50, '.')
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 6, 17, '.')
+        self.sisfin_escrever(6, 17, '.')
         time.sleep(0.5)
 
-    def vmcb_acessar(self, screen, periodo, opcao):
+    def vmcb_acessar(self, periodo, opcao):
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 21,8,'VMCB')
+        self.sisfin_escrever(21, 8, 'VMCB')
         time.sleep(1)
-        while (self.sisfin_ler(screen, 2, 1,7) == 'Cardapio'):
+        while (self.sisfin_ler(2, 1,7) == 'Cardapio'):
             time.sleep(1)
 
-        self.sisfin_escrever(screen, 4, 12, periodo)
+        self.sisfin_escrever(4, 12, periodo)
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 5, 12, opcao)
+        self.sisfin_escrever(5, 12, opcao)
         time.sleep(1.5)
 
         contagem = 0
-        while self.sisfin_ler(screen, 9, 1, 2).strip() == '' and contagem <= 4:
+        while self.sisfin_ler(9, 1, 2).strip() == '' and contagem <= 4:
             time.sleep(0.5)
             contagem += 1
 
-    def vmcb_tratamento_sem_movimentacao(self, screen, celula_planilha):
-        celula_planilha = 0
+    def vmcb_tratamento_sem_movimentacao(self):
         print("Não há movimentações nesta data...")
         print("")
-        self.sisfin_escrever(screen, 5, 12, '.')
+        self.sisfin_escrever(5, 12, '.')
 
-    def vmcd_acessar_detalhando_menu_opcao(self, screen, periodo, opcao):
-        self.sisfin_escrever(screen, 21,8,'VMCD')
+    def vmcd_acessar_detalhando_menu_opcao(self, periodo, opcao):
+        self.sisfin_escrever(21,8,'VMCD')
         time.sleep(1)
-        while (self.sisfin_ler(screen, 2, 1,7) == 'Cardapio'):
+        while (self.sisfin_ler(2, 1,7) == 'Cardapio'):
             time.sleep(1)
 
-        self.sisfin_escrever(screen, 4, 17, 'CEF')
+        self.sisfin_escrever(4, 17, 'CEF')
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 5, 17, periodo)
+        self.sisfin_escrever(5, 17, periodo)
         time.sleep(1)
 
-        self.sisfin_escrever(screen, 21, 8, '10')
+        self.sisfin_escrever(21, 8, '10')
         time.sleep(1.5)
 
-    def vmcd_acessar_sem_detalhar_menu_opcao(self, screen, periodo):
+    def vmcd_acessar_sem_detalhar_menu_opcao(self, periodo):
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 21,8,'VMCD')
+        self.sisfin_escrever(21,8,'VMCD')
         time.sleep(1)
-        while (self.sisfin_ler(screen, 2, 1,7) == 'Cardapio'):
+        while (self.sisfin_ler(2, 1,7) == 'Cardapio'):
             time.sleep(1)
 
-        self.sisfin_escrever(screen, 4, 17, 'CEF')
+        self.sisfin_escrever(4, 17, 'CEF')
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 5, 17, periodo)
+        self.sisfin_escrever(5, 17, periodo)
         time.sleep(1.5)
 
-    def vmcd_tratamento_sem_movimentacao(self, screen, celula_planilha):
+    def vmcd_tratamento_sem_movimentacao(self, celula_planilha):
         print("Não há movimentações nesta data...")
         celula_planilha = 0 
         print("")
-        self.sisfin_escrever(screen, 21, 8, '.')
+        self.sisfin_escrever(21, 8, '.')
 
-    def veca_acessar(self, screen, periodo):
+    def veca_acessar(self, periodo):
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 21,8,'VECA')
+        self.sisfin_escrever(21,8,'VECA')
         time.sleep(1)
-        while (self.sisfin_ler(screen, 2, 1,7) == 'Cardapio'):
+        while (self.sisfin_ler(2, 1,7) == 'Cardapio'):
             time.sleep(1)
 
-        self.sisfin_escrever(screen, 4, 17, 'CEF')
+        self.sisfin_escrever(4, 17, 'CEF')
         time.sleep(0.5)
-        self.sisfin_escrever(screen, 5, 17, periodo)
+        self.sisfin_escrever(5, 17, periodo)
         time.sleep(1.5)
 
-    def veca_tratamento_sem_movimentacao(self, screen, celula_planilha):
+    def veca_tratamento_sem_movimentacao(self, celula_planilha):
         print("Sem movimentação nesta data")
         print("")
         celula_planilha = 0
-        self.sisfin_escrever(screen, 21, 8, '.')
+        self.sisfin_escrever(21, 8, '.')
         time.sleep(0.5)
+
+    def enter(self):
+        return self.screen.SendKeys("<Enter>")
 
     def tirar_printscreen(self, tipo_relatorio, nome_arquivo):
         if tipo_relatorio != 'parcial':
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import pyodbc
 from classes.sisfin import Sisfin
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from credenciais import *
 import getpass
 import os
@@ -13,6 +13,16 @@ def capturar_dados_sisfin_saques_depositos_bb_bacen():
     
     usuario = input('Matrícula: ')
     senha = getpass.getpass('Senha: ')
+    data_hora_consulta = str(f"{datetime.now():%Y-%m-%d %H:%M:%S}")
+    sisfin = Sisfin(usuario, senha)
+
+    conexao_banco = pyodbc.connect(f"DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USER};PWD={PASSWORD}")
+    cursor = conexao_banco.cursor()
+
+    if not cursor.tables(table='TP_90003_03_TEMP_SAQUE_DEPOSITO_BB_BACEN', tableType='TABLE').fetchone():
+        consulta = "CREATE TABLE [dbo].[TP_90003_03_TEMP_SAQUE_DEPOSITO_BB_BACEN]([dtDataMovimento] [date], [vcTipoMovimentacao] [varchar](10), [chLinhaVmcbSisfin] [char](3), [inCodigoUnidade] [int], [vcNomeUnidade] [varchar](max), [chLM] [char](2), [dcValorSaque] [decimal](17,2), [vcSL] [varchar](10), [dtDataCaptura] [datetime])"
+        cursor.execute(consulta)
+        conexao_banco.commit()
 
     for dia in range(-1, 11):
         if dia == -1:
@@ -25,31 +35,102 @@ def capturar_dados_sisfin_saques_depositos_bb_bacen():
             periodo = f"H+{dia}"
             data_periodo = date.today() + timedelta(days=dia)
 
-    sisfin = Sisfin(usuario, senha)
+        print("")
+        escrever_cabecalho('SAQUE BB/BACEN')
+        sisfin.vmcb_acessar(periodo, 'S')
+        tela = 1
+        terminou_agencias = False
+        tipo_movimentacao = "SAQUE"
+        # TRATAMENTO CASO NÃO EXISTA MOVIMENTAÇÃO NO DIA
+        if  sisfin.sisfin_ler(5, 12, 4).strip() != 'Saque':
+            sisfin.vmcb_tratamento_sem_movimentacao()
+        else:
+            while not terminou_agencias:
+                linha = 9
+                for passo in range(1, linha + 2):
+                    time.sleep(0.5)
+                    if sisfin.sisfin_ler(linha, 6, 4).strip() != '':
+                        codigo_vmcb_sisfin = sisfin.sisfin_ler(linha, 1, 2).strip()
+                        codigo_unidade = sisfin.sisfin_ler(linha, 6, 3).strip()
+                        nome_unidade = sisfin.sisfin_ler(linha, 13, 32).strip()
+                        lm = sisfin.sisfin_ler(linha, 47, 1).strip()
+                        valor_saque = sisfin.sisfin_ler(linha, 50, 17).strip().replace(".", "").replace(",", ".")
+                        sl = sisfin.sisfin_ler(linha, 69, 3).strip()
+                        realiza_insert_banco(conexao_banco, cursor, data_periodo, tipo_movimentacao, codigo_vmcb_sisfin, codigo_unidade, nome_unidade, lm, valor_saque, sl, data_hora_consulta)
+                        print(f"Linha: {codigo_vmcb_sisfin} | Código Unidade: {codigo_unidade} | Nome Unidade: {nome_unidade} | LM: {lm} | Valor do Saque: R$ {valor_saque} | SL: {sl}")
+                    else:
+                        terminou_agencias = True
+                        break
+                    linha += 1
+                sisfin.enter()
+                tela += 1
+            sisfin.sisfin_voltar_pagina_inicial_cardapio(20)
 
-
+        print("")
+        escrever_cabecalho('DEPOSITO BB/BACEN')
+        sisfin.vmcb_acessar(periodo, 'D')
+        # TRATAMENTO CASO NÃO EXISTA MOVIMENTAÇÃO NO DIA
+        sem_movimentacao = sisfin.sisfin_ler(5, 12, 7).strip() != 'Deposito'
+        if sem_movimentacao:
+            sisfin.vmcb_tratamento_sem_movimentacao()
+        else:
+            terminou_agencias = False
+            tipo_movimentacao = "DEPOSITO"
+            while not terminou_agencias:
+                linha = 9
+                for passo in range(1, linha + 2):
+                    time.sleep(0.5)
+                    if sisfin.sisfin_ler(linha, 6, 3).strip() != '':
+                        codigo_vmcb_sisfin = sisfin.sisfin_ler(linha, 1, 2).strip()
+                        codigo_unidade = sisfin.sisfin_ler(linha, 6, 3).strip()
+                        nome_unidade = sisfin.sisfin_ler(linha, 13, 32).strip()
+                        lm = sisfin.sisfin_ler(linha, 47, 1).strip()
+                        valor_saque = sisfin.sisfin_ler(linha, 50, 17).strip().replace(".", "").replace(",", ".")
+                        sl = sisfin.sisfin_ler(linha, 69, 3).strip()
+                        realiza_insert_banco(conexao_banco, cursor, data_periodo, tipo_movimentacao, codigo_vmcb_sisfin, codigo_unidade, nome_unidade, lm, valor_saque, sl, data_hora_consulta)
+                        print(f"Linha: {codigo_vmcb_sisfin} | Código Unidade: {codigo_unidade} | Nome Unidade: {nome_unidade} | LM: {lm} | Valor do Depósito: R$ {valor_saque} | SL: {sl}")
+                    else:
+                        terminou_agencias = True
+                        break
+                    linha += 1
+            sisfin.enter()
+            sisfin.sisfin_voltar_pagina_inicial_cardapio(20)
 
     sisfin.sisfin_finalizar()
 
 
-
-
-    # conn = pyodbc.connect(f"DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USER};PWD={PASSWORD}")
-
-    # # cursor = conn.cursor()
-
-    # string = "CREATE TABLE TestTable(symbol varchar(15), leverage double, shares integer, price double)"
-    # cur.execute(string)
-
-
-    # for row in cursor:
-    #     print(row)
-
 def escrever_cabecalho(mensagem):
-    print("{:*^100}" .format("*"))
-    print("{:*^100}" .format(f" {mensagem} "))
-    print("{:*^100}" .format("*"))
+    print("{:*^120}" .format("*"))
+    print("{:*^120}" .format(f" {mensagem} "))
+    print("{:*^120}" .format("*"))
     print("")
+
+
+def realiza_insert_banco(conexao_banco, cursor, data_periodo, tipo_movimentacao, codigo_vmcb_sisfin, codigo_unidade, nome_unidade, lm, valor_saque, sl, data_hora_consulta):
+    consulta = """
+        INSERT INTO [dbo].[TP_90003_03_TEMP_SAQUE_DEPOSITO_BB_BACEN]
+           ([dtDataMovimento]
+           ,[vcTipoMovimentacao]
+           ,[chLinhaVmcbSisfin]
+           ,[inCodigoUnidade]
+           ,[vcNomeUnidade]
+           ,[chLM]
+           ,[dcValorSaque]
+           ,[vcSL]
+           ,[dtDataCaptura])
+        VALUES
+            ('{}'
+            ,'{}'
+            ,'{}'
+            ,'{}'
+            ,'{}'
+            ,'{}'
+            ,'{}'
+            ,'{}'
+            ,'{}')
+        """
+    cursor.execute(consulta.format(data_periodo, tipo_movimentacao, codigo_vmcb_sisfin, codigo_unidade, nome_unidade, lm, valor_saque, sl, data_hora_consulta))
+    conexao_banco.commit()
 
 
 if __name__ == "__main__":
